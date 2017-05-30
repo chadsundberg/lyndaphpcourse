bea34cd5e970010d22381e36fe84ffe73a26713e
Chad Sundberg <chadsundberg@gmail.com>
2017-05-30 18:39:52 -0500

    first commit

---
+++ display_values.php
@@ -0,0 +1,13 @@
+<?php
+$firstName = 'David';
+$prenom = 'David';
+//$first-Name = 'David';
+//$1stName = 'David';
+$number = 4200.99;
+
+//echo $number, ' ', $firstName;
+//print $number;
+echo $prenom;
+
+
+
